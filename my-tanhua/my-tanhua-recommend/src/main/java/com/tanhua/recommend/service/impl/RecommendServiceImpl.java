f4ab37770e32eae70e9a9d8f6f1399bde95b31e6
Will Dufresne <chxdtc123@gmail.com>
2021-01-05 14:06:43 +0800

    another commit

--- my-tanhua/my-tanhua-recommend/src/main/java/com/tanhua/recommend/service/impl/RecommendServiceImpl.java
+++ my-tanhua/my-tanhua-recommend/src/main/java/com/tanhua/recommend/service/impl/RecommendServiceImpl.java
@@ -18,8 +18,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class RecommendServiceImpl implements RecommendService {
@@ -59,62 +58,73 @@ public class RecommendServiceImpl implements RecommendService {
 
     @Override
     public RecommendUsers findRecommendUsers(String token, QueryUser queryUser) {
+        // 先解析token，获取当前登录用户的ID
         Long id = TokenUtil.parseToken2Id(token);
 
+        // 从参数中拿到分页相关参数
         Integer startPage = queryUser.getPage();
         Integer pageSize = queryUser.getPagesize();
+        // 起始页码减一
         PageRequest pageRequest = PageRequest.of(startPage - 1, pageSize);
 
+        // 根据登录用户的ID，去MongoDB中，查询所有推荐给当前用户的用户，即推荐列表
         Query query = Query.query(Criteria.where("toUserId").is(id)).with(pageRequest)
                 .with(Sort.by(Sort.Order.desc("score")));
         List<RecommendUser> recommendUserList = mongoTemplate.find(query, RecommendUser.class);
 
-        RecommendUsers recommendUsers = new RecommendUsers();
+        // 拿到推荐列表中，所有用户的ID，组成ID集合
+        Set<Long> idSet = new HashSet<>();
+        for (RecommendUser recommendUser : recommendUserList) {
+            Long userId = recommendUser.getUserId();
+            idSet.add(userId);
+        }
 
-//        List<UserInfo> users = new ArrayList<>();
+        // 从参数中，获取当前登录用户的筛选条件
+        String city = queryUser.getCity();
+        String education = queryUser.getEducation();
+        Integer age = queryUser.getAge();
+        String genderStr = queryUser.getGender();
+        String lastLogin = queryUser.getLastLogin();
+
+        // 根据筛选条件，去MySQL中，查询符合条件的用户
+        QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+
+        queryWrapper.in("user_id", idSet);
+
+        if (StringUtils.isNotEmpty(education) && StringUtils.isNotBlank(education)) {
+//                queryWrapper.eq("edu", education);
+        }
+
+        if (age != null) {
+//            queryWrapper.le("age", age);
+        }
+
+        if (StringUtils.isNotEmpty(genderStr) && StringUtils.isNotBlank(genderStr)) {
+//                Integer gender = genderStr.equals("男") ? 1 : 2;
+//                queryWrapper.eq("sex", gender);
+        }
+
+        if (StringUtils.isNotEmpty(city) && StringUtils.isNotBlank(city)) {
+//                queryWrapper.like("city", city);
+        }
+
+        // 筛选出符合条件的新的推荐列表
+        List<UserInfo> userInfos = userInfoMapper.selectList(queryWrapper);
 
         List<TodayBest> todayBests = new ArrayList<>();
 
-        for (RecommendUser recommendUser : recommendUserList) {
-
-
+        // 把符合条件的推荐列表中的用户信息，添加到返回值集合中
+        for (UserInfo userInfo : userInfos) {
+            // 但是UserInfo表中，没有缘分值相关数据，前台又需要这个数据，需要去MongoDB中查询
+            Long userId = userInfo.getUserId();
+            Query idQuery = Query.query(Criteria.where("userId").is(userId));
+            RecommendUser recommendUser = mongoTemplate.findOne(idQuery, RecommendUser.class);
             Double score = Math.floor(recommendUser.getScore());
             long value = score.longValue();
 
             TodayBest todayBest = new TodayBest();
+            // 对返回对象赋值，包括缘分值等，并添加到集合中
             todayBest.setFateValue(value);
-
-            String city = queryUser.getCity();
-            String education = queryUser.getEducation();
-            Integer age = queryUser.getAge();
-            String genderStr = queryUser.getGender();
-            String lastLogin = queryUser.getLastLogin();
-
-            Long userId = recommendUser.getUserId();
-
-            QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
-
-            queryWrapper.in("user_id", userId);
-
-            if (StringUtils.isNotEmpty(education) && StringUtils.isNotBlank(education)) {
-//                queryWrapper.eq("edu", education);
-            }
-
-            if (age != null) {
-//                queryWrapper.le("age", age);
-            }
-
-            if (StringUtils.isNotEmpty(genderStr) && StringUtils.isNotBlank(genderStr)) {
-//                Integer gender = genderStr.equals("男") ? 1 : 2;
-//                queryWrapper.eq("sex", gender);
-            }
-
-            if (StringUtils.isNotEmpty(city) && StringUtils.isNotBlank(city)) {
-//                queryWrapper.like("city", city);
-            }
-
-            UserInfo userInfo = userInfoMapper.selectOne(queryWrapper);
-
             todayBest.setId(userInfo.getUserId());
             todayBest.setAge(userInfo.getAge());
             todayBest.setAvatar(userInfo.getLogo());
@@ -128,6 +138,10 @@ public class RecommendServiceImpl implements RecommendService {
             return null;
         }
 
+        //按照缘分值进行倒序排序
+        Collections.sort(todayBests, (o1, o2) -> new Long(o2.getFateValue() - o1.getFateValue()).intValue());
+
+        RecommendUsers recommendUsers = new RecommendUsers();
         recommendUsers.setPage(startPage);
         recommendUsers.setPagesize(pageSize);
         recommendUsers.setCounts(todayBests.size());
